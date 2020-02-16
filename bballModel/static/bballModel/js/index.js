1638736f016041708e75cc51572dafbba5118dee
fennellSAP <alexander.fennell@sap.com>
2020-02-16 13:26:13 -0600

    adding bracket to UI

--- bballModel/static/bballModel/js/index.js
+++ bballModel/static/bballModel/js/index.js
@@ -1,4 +1,22 @@
+function toViewResults() {
 
+    document.getElementById("viewResultsDiv").style.display = "block";
+    document.getElementById("viewResultsTag").style.textDecoration = "underline";
+
+    document.getElementById("createModelDiv").style.display = "none";
+    document.getElementById("createModelTag").style.textDecoration = "none";
+    
+}
+
+function toCreateModel() {
+
+    document.getElementById("viewResultsDiv").style.display = "none";
+    document.getElementById("viewResultsTag").style.textDecoration = "none";
+
+    document.getElementById("createModelDiv").style.display = "block";
+    document.getElementById("createModelTag").style.textDecoration = "underline";
+
+}
 
 
 
@@ -19,7 +37,7 @@ function resetUI() {
 
 function calculate() {
 
-    stats = ["SOS", "RPI", "PythW", "last10", "top25", "nonConf", "wp", "awp", "awayWP", "owp", "oowp", "pace", "ts", "efg", "ortg", "drtg", "nrtg", "ftr", "tovr", "dm", "orb", "erb", "blkp", "aps", "apa", "apd", "afbp", "ascp", "apip", "3pp", "ftp", "fgp", "aa", "avgs", "at", "ab", "aorb"];
+    stats = ["SOS", "RPI", "PythW", "last10", "top25", "nonConf", "wp", "awp", "awayWP", "owp", "oowp", "pace", "ts", "efg", "ortg", "drtg", "nrtg", "ftr", "tovr", "dm", "orb", "erb", "blkp", "aps", "apa", "apd", "afbp", "ascp", "apip", "threepp", "ftp", "fgp", "aa", "avgs", "at", "ab", "aorb"];
     weightsDict = {}
 
     var spans = document.getElementsByClassName('range-slider__value');
@@ -45,12 +63,17 @@ function calcModel(jsonModel) {
         },
         dataType: 'json',
         success: function (data) {
-            console.log(data);
+            displayResults(data);
         },
         error: function(error) {
             console.error(error);
         }
     
     });
+}
+
+function displayResults(data) {
+    toViewResults();
+    console.log(data);
 }
 
--- bballModel/views.py
+++ bballModel/views.py
@@ -4,6 +4,7 @@ from .getDataCalls import dataController
 import json
 import operator
 from .models import Team
+import random
 
 def index(request):
 
@@ -20,24 +21,67 @@ def calcModel_view(request):
 
     # Init dictionary to hold each teams score
     for team in teams:
-        teamScoreDict[team.teamID] = 0
+        teamScoreDict[team.teamID] = {"name":team.name, "score":0}
 
+    # Sum team score for each weight specified
     for weight in weightsDict:
         if int(weightsDict[weight]["weight"]) > 0:
             teamScoreDict = addWeightScore(teamScoreDict, teams, int(weightsDict[weight]["weight"]), weightsDict[weight]["stat"])
-            
-    return JsonResponse({'message':'received'})
+
+    results = determineWinners(teams, teamScoreDict) 
+
+    #jsonResults = json.dumps(results)
+    
+    return JsonResponse(results, safe=False)
 
 def addWeightScore(teamScoreDict, teams, weight, stat):
 
-    orderedTeams = sorted(teams, key=operator.attrgetter(stat))
+    if stat == "tovr" or stat == "apa" or stat == "at":
+        orderedTeams = sorted(teams, key=operator.attrgetter(stat), reverse=True)
+    else:
+        orderedTeams = sorted(teams, key=operator.attrgetter(stat))
+
     
     for i,team in enumerate(orderedTeams):
         
-        weightScore = (i / len(orderedTeams)) * weight
-        teamScoreDict[team.teamID] += weightScore
+        weightScore = (i / (len(orderedTeams) - 1)) * weight
+        teamScoreDict[team.teamID]["score"] += weightScore
     
     return teamScoreDict
+
+def determineWinners(teams, teamScoreDict):
+
+    results = [[]]
+
+    for team in teams:
+        results[0].append(teamScoreDict[team.teamID]) 
+
+    return winnerHelper(results, 1)
+
+
+def winnerHelper(results, roundNum):
+
+    if roundNum == 6: return results
+
+    results.append([])
+
+    for i,team in enumerate(results[roundNum - 1]):
+        if i % 2 == 0: continue
+
+        if team["score"] > results[roundNum - 1][i - 1]["score"]:
+            results[roundNum].append(team)
+        elif team["score"] < results[roundNum - 1][i - 1]["score"]:
+            results[roundNum].append(results[roundNum - 1][i - 1])
+
+        # Somehow a tie...very low probability
+        # Randomly choose winner
+        else:
+            randNum = random.randint(0,1)
+            if randNum == 0: results[roundNum].append(team)
+            else: results[roundNum].append(results[roundNum - 1][i - 1])
+
+
+    return winnerHelper(results, roundNum + 1)
 
 
 
